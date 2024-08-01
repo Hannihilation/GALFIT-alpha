477c8626d69f8b1924fa831943d2a52b87cd025f
Hannihilation <hzx20000818qaz@163.com>
2024-08-01 11:05:35 +0800

    add env

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Galfit Alpha
 
 ## TODO
-- [ ] RL输入是什么，residual和model两个图像？
+- [ ] RL输入的state是什么，residual和model两个图像？加上参数 1D
 - [ ] 图片大小不一怎么解决
 - [ ] residual要mask吗
-- [ ] RL输出什么，1不再增加comp 2增加一个comp 3 增加一个但index不可调
+- [ ] RL输出的action什么，1不再增加comp 2增加一个comp 3 增加一个但index不可调
+  - add one ser(free, fixed)
+  -
 - [ ] 增加comp后初始条件如何程序化给出
+- reward:
---
+++ env.py
@@ -0,0 +1,58 @@
+from graphviz import Digraph
+from typing import Union
+
+
+class State:
+    def __init__(self, name: str = None, reward: float = 0) -> None:
+        self._name = name
+        self._successor = []
+        self._r = reward
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def out_degree(self):
+        return len(self._successor)
+
+    @property
+    def reward(self):
+        return self._r
+
+    def add_successor(self, successor: 'State'):
+        self._successor.append(successor)
+
+    def plot(self, dot: Digraph):
+        dot.node(self.name, f'{self.name}\nr={self.reward}')
+        for successor in self._successor:
+            successor.plot(dot)
+            dot.edge(self.name, successor.name)
+
+
+class GalfitEnv:
+    def __init__(self) -> None:
+        self._root = State('root', 0)
+        self._states = [self._root]
+        self.current_state = self._root
+
+    def _add_state(self, state: State, parents: Union[State, list[State]]):
+        if isinstance(parents, list):
+            for parent in parents:
+                parent.add_successor(state)
+        else:
+            parents.add_successor(state)
+        self._states.append(state)
+
+    def plot(self):
+        dot = Digraph(comment='Galfit Environment')
+        self._root.plot(dot)
+        dot.render('fig/galfit_env', view=True, format='pdf')
+
+    def do_action(self, action: int):
+        pass
+
+
+if __name__ == '__main__':
+    env = GalfitEnv()
+    env.plot()
