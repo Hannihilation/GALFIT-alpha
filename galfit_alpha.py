6dd01baf35ac242cbac479c5884708fd6438faa7
Hannihilation <hzx20000818qaz@163.com>
2024-10-08 19:34:15 +0800

    remodel galfit alpha

--- galfit_alpha.py
+++ galfit_alpha.py
@@ -1,4 +1,5 @@
 from torch import nn, optim, cat, from_numpy, float64
+import numpy as np
 
 
 class GalfitAlpha(nn.Module):
@@ -8,28 +9,27 @@ class GalfitAlpha(nn.Module):
         self.fig_channel_num = fig_channel_num
         self.fig_size = fig_size
         self.n_action = n_action
-        # channel 0: residue, channel 1: components
-        # CNN subgraph:
-        kernel_size = fig_size // 4
-        print(kernel_size)
-        self.Conv1 = nn.Conv2d(in_channels=fig_channel_num,
-                               out_channels=5, kernel_size=(5, 5), dtype=float64)
-        self.Conv2 = nn.Conv2d(
-            in_channels=5, out_channels=20, kernel_size=(kernel_size, kernel_size), dtype=float64)
-        self.Conv3 = nn.Conv2d(
-            in_channels=20, out_channels=10, kernel_size=(fig_size - 3 * kernel_size + 2, fig_size - 3 * kernel_size + 2), dtype=float64)
-        # State all-connected subgraph:
-        self.LL1 = nn.Linear(in_features=state_num,
-                             out_features=5, dtype=float64)
-        self.LL2 = nn.Linear(in_features=5, out_features=5, dtype=float64)
-        self.LL3 = nn.Linear(in_features=5, out_features=5, dtype=float64)
-        # Adding-up layer
-        self.OutL1 = nn.Linear(
-            in_features=15, out_features=30, dtype=float64)
-        self.OutL2 = nn.Linear(
-            in_features=30, out_features=20, dtype=float64)
-        self.OutL3 = nn.Linear(
-            in_features=20, out_features=n_action, dtype=float64)
+
+        self.convs = nn.Sequential()
+        layer_num = int(np.log2(fig_size))
+        channel_now = fig_channel_num
+        for _ in range(layer_num):
+            channel_next = channel_now * 2
+            if channel_next > 16:
+                channel_next = 16
+            self.convs.add_module('conv', nn.Conv2d(
+                channel_now, channel_next, 3, 1, 1))
+            channel_now = channel_next
+            self.convs.add_module('relu', nn.ReLU())
+            self.convs.add_module('maxpool', nn.MaxPool2d(2, 2))
+
+        self.linears = nn.Sequential(nn.Linear(state_num+channel_now, 32),
+                                     nn.ReLU(),
+                                     nn.Linear(32, 64),
+                                     nn.ReLU(),
+                                     nn.Linear(64, 32),
+                                     nn.ReLU(),
+                                     nn.Linear(32, n_action))
 
         self.test_linear = nn.Linear(2, 5, dtype=float64)
         self.ReLU = nn.ReLU()
@@ -44,19 +44,10 @@ class GalfitAlpha(nn.Module):
         figs = from_numpy(figs).to(self.device)
         print(figs.shape, state_code.shape)
         print(figs.dtype, state_code.dtype)
-        x1 = self.ReLU(self.Conv1(figs))
-        x1 = self.ReLU(self.Conv2(x1))
-        x1 = self.ReLU(self.Conv3(x1))
-        x1 = x1.view(x1.size(0), -1)
-        # LinearNN for state code
-        x2 = self.ReLU(self.LL1(state_code))
-        x2 = self.ReLU(self.LL2(x2))
-        x2 = self.ReLU(self.LL3(x2))
-        # OutputNN
-        x = cat(x1, x2, dim=1)
-        x = self.ReLU(self.OutL1(x))
-        x = self.ReLU(self.OutL2(x))
-        x = self.OutL3(x)
+        x = self.convs(figs)
+        x = x.view(x.size(0), -1)
+        x = cat(x, state_code, dim=1)
+        x = self.linears(x)
         return x
 
     def forward(self, state_code, figs):
